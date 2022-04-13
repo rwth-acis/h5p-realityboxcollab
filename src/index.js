1a133aff8db4f5d3825f4a74a8e36701cd13e1e6
Konstantin Kühlem <konstantin@koeln.de>
2022-04-13 12:17:25 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import RealityBoxCollab from './js/realityboxcollab';
+
+// Load library
+H5P = H5P || {};
+H5P.RealityBoxCollab = RealityBoxCollab;
---
+++ src/js/realityboxcollab.js
@@ -0,0 +1,15 @@
+H5P.RealityBoxCollab = (function ($) {
+
+  function RealityBoxCollab(options, id) {
+    console.log("XXXXXXXXXXXXXXXXXX");
+  }
+
+  RealityBoxCollab.prototype.attach = async function ($container) {
+
+  }
+
+  return RealityBoxCollab;
+
+})(H5P.jQuery);
+
+export default H5P.RealityBoxCollab;
