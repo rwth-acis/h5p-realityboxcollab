6fcaad03c0b4dbc8506426d4d0d3030d41c272f0
Konstantin Kühlem <konstantin@koeln.de>
2022-04-13 15:28:05 +0200

    Functional Realitybox

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import RealityBoxCollab from './js/realityboxcollab';
+import './css/material-icons/material-icons.css';
+import './css/realitybox.css';
 
 // Load library
 H5P = H5P || {};
--- src/js/realityboxcollab.js
+++ src/js/realityboxcollab.js
@@ -1,14 +1,16 @@
 H5P.RealityBoxCollab = (function ($) {
 
   function RealityBoxCollab(options, id) {
-    console.log("XXXXXXXXXXXXXXXXXX");
+    this.options = options.realityboxcollab;
   }
 
   RealityBoxCollab.prototype.attach = async function ($container) {
     const realitybox = H5P.newRunnable({
-      library: 'H5P.Realitybox 1.0',
-      params
-    }, this.id, undefined, undefined, {parent: this});
+      library: 'H5P.RealityBox 1.0',
+      params: {realitybox: this.options}
+    }, this.id, undefined, undefined, { parent: this });
+
+    await realitybox.attach($container);
   }
 
   return RealityBoxCollab;
