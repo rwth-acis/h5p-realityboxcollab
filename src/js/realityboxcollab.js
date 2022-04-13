a156224dd7aa4a9bac9168c5db6ee472394978ea
Konstantin Kühlem <konstantin@koeln.de>
2022-04-13 15:44:16 +0200

    Can display box in scene

--- src/js/realityboxcollab.js
+++ src/js/realityboxcollab.js
@@ -1,3 +1,5 @@
+//import { HemisphericLight, Vector3, Vector4 } from 'babylonjs';
+
 H5P.RealityBoxCollab = (function ($) {
 
   function RealityBoxCollab(options, id) {
@@ -5,12 +7,18 @@ H5P.RealityBoxCollab = (function ($) {
   }
 
   RealityBoxCollab.prototype.attach = async function ($container) {
-    const realitybox = H5P.newRunnable({
+    this.realitybox = H5P.newRunnable({
       library: 'H5P.RealityBox 1.0',
       params: {realitybox: this.options}
     }, this.id, undefined, undefined, { parent: this });
 
-    await realitybox.attach($container);
+    await this.realitybox.attach($container);
+
+    // Tests
+    const box = BABYLON.MeshBuilder.CreateBox("test-box", {
+      size: 20
+    }, this.realitybox._viewer._babylonBox.scene);
+    box.material = new BABYLON.StandardMaterial("mat");
   }
 
   return RealityBoxCollab;
