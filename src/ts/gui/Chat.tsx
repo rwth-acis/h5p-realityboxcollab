afe8135ca3dc6d3c4a5d3caaf330defc246f6f4b
Konstantin Kühlem <konstantin@koeln.de>
2022-05-21 10:29:20 +0200

    Working on CHat

--- src/ts/gui/Chat.tsx
+++ src/ts/gui/Chat.tsx
@@ -15,8 +15,18 @@ export class Chat extends AbstractGuiElement {
       <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' />
       <div id='collabChat' className='guiElement'>
         <h1>Chat</h1>
+        <div id="chatMessageField">
+
+        </div>
+        <input id="chatInput"></input><button onClick={this.onChatSend.bind(this)}>Send</button>
       </div>
     </span>
+  }
+
+  onChatSend() {
+    let msg = (document.getElementById("chatInput") as HTMLInputElement).value;
+    console.log(msg);
+    this.sendMessage(msg);
   }
 
 
@@ -25,6 +35,8 @@ export class Chat extends AbstractGuiElement {
   }
 
   sendMessage(msg: string) {
+    $("#chatMessageField").append(msg);
+    $("#chatMessageField").append("<br>");
     this.currentRoom.user.messages.push(new ChatMessage(msg));
     this.currentRoom.applyUpdate();
   }
