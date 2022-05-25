12b386997aaa4e409731baba2ad44bf1abdae87b
Konstantin Kühlem <konstantin@koeln.de>
2022-05-25 18:35:32 +0200

    Minor

--- src/ts/gui/BabylonViewer.ts
+++ src/ts/gui/BabylonViewer.ts
@@ -15,6 +15,8 @@ export class BabylonViewer extends NetworkListener {
         });
     }
 
+    // Creating lines for pointer: https://doc.babylonjs.com/divingDeeper/mesh/creation/set
+
     onRender() {
         if (!this.currentRoom) return;
 
@@ -40,19 +42,51 @@ export class BabylonViewer extends NetworkListener {
 
 }
 
-const RED = new BABYLON.Color4(1, 0, 0, 1);
-const GREEN = new BABYLON.Color4(0, 1, 0, 1);
-const BLUE = new BABYLON.Color4(0, 0, 1, 1);
+const RED = new BABYLON.Color3(1, 0, 0);
+const GREEN = new BABYLON.Color3(0, 1, 0);
+const BLUE = new BABYLON.Color3(0, 0, 1);
 
 class UserMesh {
 
     mesh: BABYLON.Mesh;
+    static matHost: BABYLON.StandardMaterial;
+    static matUser: BABYLON.StandardMaterial;
 
     constructor(private user: User, scene: BABYLON.Scene) {
-        this.mesh = BABYLON.MeshBuilder.CreateBox("test-box", {
-            size: 20,
-            faceColors: user.role == Role.HOST ? [RED, RED, RED, RED, RED, RED] : [BLUE, BLUE, BLUE, BLUE, BLUE, BLUE]
+        if (!UserMesh.matHost) UserMesh.createMats(scene);
+
+        this.mesh = BABYLON.MeshBuilder.CreateCapsule(user.username, {
+            height: 32,
+            radius: 8,
+            subdivisions: undefined,
+            capSubdivisions: undefined,
+            tessellation: undefined
         }, scene);
-        this.mesh.material = new BABYLON.StandardMaterial("mat", scene);
+        this.mesh.material = user.role == Role.HOST ? UserMesh.matHost : UserMesh.matUser;
+
+        /*var plane = BABYLON.Mesh.CreatePlane("plane", 2, scene);
+        plane.parent = this.mesh;
+        plane.position.y = 2;
+        var advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
+
+        var button1 = Button.CreateSimpleButton("but1", this.user.username);
+        button1.width = 1;
+        button1.height = 0.4;
+        button1.color = "gray";
+        button1.fontSize = 50;
+        button1.background = "green";
+        button1.onPointerUpObservable.add(function () {
+            alert("you did it!");
+        });
+        advancedTexture.addControl(button1);*/
     }
+
+    static createMats(scene: BABYLON.Scene) {
+        UserMesh.matHost = new BABYLON.StandardMaterial("matHost", scene);
+        UserMesh.matUser = new BABYLON.StandardMaterial("matUser", scene);
+
+        UserMesh.matHost.diffuseColor = BLUE;
+        UserMesh.matUser.diffuseColor = RED;
+    }
+
 }
