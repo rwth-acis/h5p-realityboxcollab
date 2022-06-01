a423fa66947e6f84b79c03668e106c66e278352d
Konstantin Kühlem <konstantin@koeln.de>
2022-06-01 18:11:13 +0200

    Ball Pointer

--- src/ts/tools/PointerTool.ts
+++ src/ts/tools/PointerTool.ts
@@ -6,6 +6,7 @@ export class PointerTool extends AbstractTool {
     static readonly LINE_COLOR = new BABYLON.Color4(1, 0, 0);
 
     line: BABYLON.LinesMesh;
+    sphere: BABYLON.Mesh;
 
     constructor() {
         super("Pointer Tool", "fa-solid fa-person-chalkboard");
@@ -20,26 +21,40 @@ export class PointerTool extends AbstractTool {
                 pos.y -= 0.4;
 
                 // https://doc.babylonjs.com/divingDeeper/mesh/interactions/picking_collisions
-                let hit = scene.pickWithRay(new BABYLON.Ray(cam.position, cam.getDirection(BABYLON.Vector3.Forward())));
+                let hit = scene.pickWithRay(new BABYLON.Ray(cam.position, cam.getDirection(BABYLON.Vector3.Forward())),
+                    mesh => RealityBoxCollab.instance.realitybox.viewer._babylonBox.model.env.getChildMeshes().find(c => c == mesh) != undefined);
                 let target;
                 if (hit) {
                     target = hit.pickedPoint;
                 }
 
-                if (!target) target = pos;
+                if (!target) {
+                    target = pos;
+                }
 
                 this.line = BABYLON.MeshBuilder.CreateLines("pointer", {
                     points: [pos, target],
                     updatable: true,
                     colors: [PointerTool.LINE_COLOR, PointerTool.LINE_COLOR],
                     instance: this.line // Update this line, instead of creating new
-                });
+                }, scene);
+                if (!this.sphere) {
+                    this.sphere = BABYLON.MeshBuilder.CreateSphere("pointerBall", {
+                        diameter: 3
+                    }, scene);
+
+                    let mat = new BABYLON.StandardMaterial("matPointerBall", scene);
+                    mat.diffuseColor = new BABYLON.Color3(1, 0, 0);
+                    this.sphere.material = mat;
+                }
+                this.sphere.position.set(target.x, target.y, target.z);
             });
     }
 
     onDeactivate(): void {
         if (this.line) {
             RealityBoxCollab.instance.realitybox.viewer._babylonBox.scene.removeMesh(this.line);
+            RealityBoxCollab.instance.realitybox.viewer._babylonBox.scene.removeMesh(this.sphere);
         }
     }
 
