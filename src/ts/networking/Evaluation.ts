812efdf2d71ecc31317c5857d314d9882a183408
Konstantin Kühlem <konstantin@koeln.de>
2022-07-22 16:32:29 +0200

    Error handling

--- src/ts/networking/Evaluation.ts
+++ src/ts/networking/Evaluation.ts
@@ -5,6 +5,7 @@ export class Evaluation {
         this.instance.babylonViewer.scene.registerBeforeRender(() => {
 
         });
+        window.onerror = this.reportError.bind(this);
 
         this.sendMetrics();
     }
@@ -13,13 +14,36 @@ export class Evaluation {
         let metrics: Metrics = {
             fps: 42
         };
+        this.send("/metrics", metrics);
+    }
+
+    private reportError(msg: string, source: string, line: number, column: number, error: any) {
+        let e: Error = {
+            message: msg,
+            source: source,
+            line: line,
+            column: column,
+            error: error
+        };
+        this.send("/error", e);
+    }
+
+    private send(endpoint: string, object: any) {
         let req = new XMLHttpRequest();
-        req.open("POST", RealityBoxCollab.EVALUATION_SERVER + "/metrics", false); // true for async
+        req.open("POST", RealityBoxCollab.EVALUATION_SERVER + endpoint, false); // true for async
         req.setRequestHeader("Content-Type", "application/json");
-        req.send(JSON.stringify(metrics));
+        req.send(JSON.stringify(object));
     }
 }
 
 interface Metrics {
     fps: number;
+}
+
+interface Error {
+    message: string;
+    source: string;
+    line: number;
+    column: number;
+    error: any;
 }
